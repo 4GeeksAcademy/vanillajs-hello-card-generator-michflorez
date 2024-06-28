2fd52e70f1f1f013e705d9e5285d7b03827c2795
Michflorez <167869106+Michflorez@users.noreply.github.com>
2024-06-28 19:15:05 +0000

    card-generator-mich-florez

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import { bottom } from "@popperjs/core";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const signs = ["&hearts;", "&diams;", "&clubs;", "&spades;"];
+  const numbers = ["A", "J", "K", "Q", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+  const randomSing = getRandomElement(signs);
+  const RandomNumber = getRandomElement(numbers);
+
+  let bottomSign = document.querySelector(".bottom-sign");
+  let topSign = document.querySelector(".top-sign");
+  let number = document.querySelector(".number");
+
+  bottomSign.innerHTML = randomSing;
+  console.log(randomSing);
+
+  topSign.innerHTML = randomSing;
+  console.log(randomSing);
+
+  number.innerHTML = RandomNumber;
+  console.log(RandomNumber);
+
+  if (randomSing == "&hearts;" || randomSing == "&diams;") {
+    bottomSign.style.color = "red";
+    topSign.style.color = "red";
+    number.style.color = "red";
+  }
 };
